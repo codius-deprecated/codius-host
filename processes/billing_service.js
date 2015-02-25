555992aeda247130e52bdcc7b6ca225937925652
wilsonianb <bwilson@ripple.com>
2015-02-25 11:10:44 -0800

    [FEATURE] Enable billing service by default Removed billing_service feature flag [Finishes #89122456]

--- processes/billing_service.js
+++ processes/billing_service.js
@@ -1,13 +1,10 @@
 
 module.exports = function(codius) {
 
-  if (codius.features.isEnabled('BILLING_SERVICE')) {
+  var server = require(__dirname+'/../billing-service/server')(codius)
+  var port   = (parseInt(process.env.PORT) || 5000) + 15
 
-    var server = require(__dirname+'/../billing-service/server')(codius)
-    var port   = (parseInt(process.env.PORT) || 5000) + 15
-
-    server.listen(port, function() {
-      codius.logger.info('Codius Billing Service listening on port', port)
-    })
-  }
+  server.listen(port, function() {
+    codius.logger.info('Codius Billing Service listening on port', port)
+  })
 }
