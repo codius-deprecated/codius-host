4262d33e37e4d1ad3c0e8772bbbc82eb25fe1341
Torrie Fischer <torrie@ripple.com>
2015-02-26 14:39:08 -0800

    Deploy to testnet from circleci

---
+++ deploy-to-testnet.sh
@@ -0,0 +1,5 @@
+git clone git@github.com:tdfischer/rippled-skynet skynet
+cd skynet
+git submodule update --init
+chmod a-rwx,u+r keys/*
+ansible-playbook -i ec2.py -e codius_version=$CIRCLE_SHA1 codius-host-docker.yml
