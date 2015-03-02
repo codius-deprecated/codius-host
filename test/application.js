aa7f68d0fe6b5c4016eb882ba6d9fb01eb69c0d2
Torrie Fischer <torrie@ripple.com>
2015-03-02 15:10:29 -0800

    Use supertest-as-promised to better catch promise errors

--- test/application.js
+++ test/application.js
@@ -2,7 +2,7 @@ var CodiusHost = require(__dirname+'/../');
 var express    = require('express');
 var sinon      = require('sinon');
 var assert     = require('assert');
-var supertest  = require('supertest');
+var supertest  = require('supertest-as-promised');
 var Contract   = require(__dirname+'/../models/contract').model;
 
 describe('Codius Host Express Application', function() {
@@ -17,50 +17,38 @@ describe('Codius Host Express Application', function() {
     assert.strictEqual(typeof application.listen, 'function');
   });
 
-  it('should expose a health check route', function(done) {
-    http
+  it('should expose a health check route', function() {
+    return http
       .get('/health')
-      .expect(200)
-      .end(function(error, response) {
-        assert.strictEqual(response.statusCode, 200);
-        done();
-      });
+      .expect(200);
   });
 
-  it('should expose a token generation route', function(done) {
+  it('should expose a token generation route', function() {
     var contractHash = '427c7a0bfa92621f93fac7ed35e42a6d4fc4fef522b89ade12776367399014ef';
-    new Contract({hash: contractHash}).save().then(function (contract) {
-      http
+    return new Contract({hash: contractHash}).save().then(function (contract) {
+      return http
         .post('/token?contract='+contractHash)
         .expect(200)
-        .end(function(error, response) {
-          assert.strictEqual(response.statusCode, 200);
+        .then(function(response) {
           token = response.body.token;
-          contract.destroy().then(function() {
-            done();
-          });
+          return contract.destroy();
         });
     })
   });
 
-  it('should expose a contract metadata route', function(done) {
-    http
+  it('should expose a contract metadata route', function() {
+    return http
       .get('/token/'+token)
-      .expect(200)
-      .end(function(error, response) {
-        assert.strictEqual(response.statusCode, 200);
-        done();
-      });
+      .expect(200);
   });
 
-  it.skip('should not upload an empty contract', function(done) {
-    http
+  it.skip('should not upload an empty contract', function() {
+    return http
       .post('/contract')
-      .end(function(error, response) {
+      .expect(500)
+      .then(function(response) {
         assert.strictEqual(response.body.success, false);
         assert.strictEqual(response.body.error, 'no contract provided');
-        assert.strictEqual(response.body.statusCode, 500);
-        done();
       });
   });
 });
--- test/http/compute_service.js
+++ test/http/compute_service.js
@@ -5,18 +5,17 @@ Promise.longStackTraces();
 var assert    = require('assert')
 var _         = require('lodash')
 var path      = require('path')
-var supertest = require('supertest')
+var supertest = require('supertest-as-promised')
 var uuid      = require('uuid')
 var codius    = require(path.join(__dirname, '/../../'))
 var engine    = require(path.join(__dirname, '/../../lib/engine'))
-var server    = require(path.join(__dirname, '/../../compute-service/server'))(codius)
+var Server    = require(path.join(__dirname, '/../../compute-service/server'));
 var Contract  = require(path.join(__dirname, '/../../models/contract')).model
-var http      = supertest(server)
 
 describe('Compute Service HTTP Interface', function() {
-  var contract, token, contractHash;
+  var contract, token, contractHash, server, http;
 
-  before(function(done) {
+  before(function() {
     var fileManager = engine.fileManager;
     var compiler = engine.compiler;
     var currentDir = path.join(__dirname, '/../test_contract');
@@ -53,18 +52,23 @@ describe('Compute Service HTTP Interface', function() {
       })
     );
 
-    Promise.all(p).then(function() {done()}).catch(function (e) {done(e)});
+    return Promise.all(p);
   });
 
-  after(function(done) {
+  beforeEach(function() {
+    server = Server(codius);
+    http = supertest(server);
+  });
+
+  after(function() {
     // TODO: Remove contract from filesystem
-    contract.destroy().then(function(){
+    return contract.destroy().then(function(){
       return token.destroy();
-    }).then(function() {done();}).catch(function(e) {done(e);});
+    });
   });
 
-  it('should start running a container', function(done) {
-    http
+  it('should start running a container', function() {
+    return http
       .post('/instances')
       .send({
         token: token.get('token'),
@@ -73,57 +77,50 @@ describe('Compute Service HTTP Interface', function() {
         // vars: 
         // port: 
       })
-      .end(function(error, response) {
-        assert(response.body.success)
+      .expect(200)
+      .then(function(response) {
         assert.strictEqual(response.body.instance.token, token.get('token'))
         assert.strictEqual(response.body.instance.container_hash, contractHash)
         assert.strictEqual(response.body.instance.state, 'running')
-        done()
-      })
+      });
   })
 
-  it('should list all running containers', function(done) {
-    http
+  it('should list all running containers', function() {
+    return http
       .get('/instances')
       .expect(200)
-      .end(function(error, response) {
-        assert(response.body.success)
+      .then(function(response) {
         var idx = _.findIndex(response.body.instances, function(instance) { return instance.token === token.get('token'); });
         assert.notStrictEqual(idx, -1)
         assert.strictEqual(response.body.instances[idx].container_hash, contractHash)
         assert.strictEqual(response.body.instances[idx].state, 'running')
-        done()
-      })
+      });
   })
 
-  it('should get info on single running container', function(done) {
-    http
+  it('should get info on single running container', function() {
+    return http
       .get('/instances/'+token.get('token'))
-      .end(function(error, response) {
-        assert(response.body.success)
+      .expect(200)
+      .then(function(response) {
         assert.strictEqual(response.body.instance.token, token.get('token'))
         assert.strictEqual(response.body.instance.container_hash, contractHash)
         assert.strictEqual(response.body.instance.state, 'running')
-        done()
-      })
+      });
   })
 
-  it('should stop a running container', function(done) {
-    http
+  it('should stop a running container', function() {
+    return http
       .delete('/instances/'+token.get('token'))
-      .end(function(error, response) {
-        assert(response.body.success)
+      .expect(200)
+      .then(function(response) {
         assert.strictEqual(response.body.instance.state, 'stopping')
-        done()
-      })
+      });
   })
 
-  it.skip('should get a quote to run a container', function(done) {
-    http
+  it.skip('should get a quote to run a container', function() {
+    return http
       .get('/quote')
-      .end(function(error, response) {
-        assert(response.body.success)
-      })
+      .expect(200);
   })
 
 })
