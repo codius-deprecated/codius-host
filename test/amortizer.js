7ac479cff85ecaec59ed709365bab88860c2e44f
Torrie Fischer <torrie@ripple.com>
2015-03-02 16:16:17 -0800

    Make amortizer tests idempotent

--- test/amortizer.js
+++ test/amortizer.js
@@ -1,3 +1,11 @@
+var Promise = require('bluebird').Promise;
+
+Promise.longStackTraces();
+var nconf      = require('../lib/config');
+nconf.set('db:connection:filename', ':memory:');
+
+var db         = require('../lib/db');
+
 var uuid       = require('uuid');
 var path       = require('path');
 var Token      = require(path.join(__dirname, '/../models/token')).model;
@@ -7,67 +15,68 @@ var assert     = require('assert');
 describe('Amortizer', function() {
   var token, startTime, startBalance;
 
-  before(function(done) {
-    startTime = Date.now();
-    startBalance = 10;
-    amortizer._instances[token] = {
-      lastCheckedBalance: startBalance,
-      lastChargedTime: startTime
-    }
-    new Token({ token: uuid.v4() }).save().then(function(token_) {
-      token = token_;
+  before(function() {
+    return db.knex.migrate.rollback(db.conf);
+  });
+
+  beforeEach(function() {
+    return db.knex.migrate.latest(db.conf).then(function() {
       startTime = Date.now();
-      amortizer._instances[token.get('token')] = {
-        lastCheckedBalance: 10,
+      startBalance = 10;
+      amortizer._instances[token] = {
+        lastCheckedBalance: startBalance,
         lastChargedTime: startTime
       }
-      token.getBalance().then(function(balance) {
-        balance.credit(10);
-        done();
-      });
-    });
-  });
-
-  after(function(done) {
-    token.destroy().then(function() {
-      done();
-    });
-  });
-
-  it('should check a token\'s balance from the database', function(done) {
-    new Token({ token: uuid.v4() }).save().then(function(token_) {
-      amortizer.checkTokenBalance(token_).then(function(balance) {
-        assert.strictEqual(balance, 0);
-      })
-      .then(function() {
-        token_.destroy().then(function() {
-          done();
+      return new Token({ token: uuid.v4() }).save().then(function(token_) {
+        token = token_;
+        startTime = Date.now();
+        amortizer._instances[token.get('token')] = {
+          lastCheckedBalance: 10,
+          lastChargedTime: startTime
+        }
+        return token.getBalance().then(function(balance) {
+          balance.credit(10);
         });
       });
     });
   });
 
-  it('should calculate the amount to charge a running instance', function(done) {
-
-    var charge = amortizer.calculateCharge(token);
-    assert(charge <= Math.ceil((Date.now() - startTime) / 100));
-    done();
-  });
-
-  it('should charge a running instance\'s balance', function(done) {
-
-    amortizer.chargeToken(token).then(function(balance) {
-      assert(startBalance - Math.ceil((Date.now() - startTime) / 100) <= balance);
-      startBalance = balance;
-      done();
+  afterEach(function() {
+    return token.destroy().then(function() {
+      return db.knex.migrate.rollback(db.conf);
     });
   });
 
-  it('should check a running instance\'s current balance', function(done) {
+  it('should check a token\'s balance from the database', function() {
+    return new Token({ token: uuid.v4() }).save().then(function(token_) {
+      return amortizer.checkTokenBalance(token_).then(function(balance) {
+        assert.strictEqual(balance, 0);
+      })
+      .then(function() {
+        return token_.destroy();
+      });
+    });
+  });
 
-    amortizer.checkTokenBalance(token).then(function(balance) {
-      assert(startBalance - Math.ceil((Date.now() - startTime) / 100) <= balance);
-      done();
+  it('should calculate the amount to charge a running instance', function() {
+    var charge = amortizer.calculateCharge(token);
+    assert(charge <= Math.ceil((Date.now() - startTime) / 100));
+  });
+
+  it('should charge a running instance\'s balance', function() {
+    return Promise.delay(10).then(function() {
+      return amortizer.chargeToken(token).then(function(balance) {
+        assert(startBalance - Math.ceil((Date.now() - startTime) / 100) <= balance);
+        startBalance = balance;
+      });
+    });
+  });
+
+  it('should check a running instance\'s current balance', function() {
+    return Promise.delay(10).then(function() {
+      return amortizer.checkTokenBalance(token).then(function(balance) {
+        assert(startBalance - Math.ceil((Date.now() - startTime) / 100) <= balance);
+      });
     });
   });
 });
