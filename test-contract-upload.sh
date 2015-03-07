0f99d6643c603eb1cd1c928c1c703a15e46765ae
Torrie Fischer <torrie@ripple.com>
2015-03-06 17:12:36 -0800

    Fix test-contract-upload.sh

--- test-contract-upload.sh
+++ test-contract-upload.sh
@@ -1,7 +1,7 @@
 expected="e9f32f730046f6522f52fbab4edb22485bfdd0e2a873e81c65337384c3d1bffe"
 cd test/test_contract
 CODIUS_UNAUTHORIZED_SSL=true CODIUS_HOST=Http://localhost:8080/ codius upload | tee /tmp/upload-output
-token=$(grep 'metadata available at' /tmp/upload-output | awk -F '/' '{print $5}')
+token=$(grep 'Application metadata' /tmp/upload-output | awk -F '/' '{print $5}')
 curl -k https://localhost:8080/token/${token} | tee /tmp/api-output
 hash=$(cat /tmp/api-output | jq .hash -r)
 
